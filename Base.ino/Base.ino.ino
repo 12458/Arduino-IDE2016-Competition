93caf0fadd3b7dbb72cb1d0eabe3b415005a104f
12458 <12458@users.noreply.github.com>
2016-03-18 14:02:57 +0800

    First Revision

---
+++ Base.ino/Base.ino.ino
@@ -0,0 +1,77 @@
+#define relay1 8
+#define relay2 9
+#define relay3 10
+#define LDR A0
+#define Buzzer 11
+#define Laser 12
+#define RX A2
+#define TX A3
+#define LDRtrigger 100
+#define delaytime 5000
+#define armedled 12
+int Buffer = 0;
+int LDRvalue = 0;
+byte arm = 0;
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(LDR, INPUT);
+  pinMode(relay1, OUTPUT);
+  pinMode(relay2, OUTPUT);
+  pinMode(relay3, OUTPUT);
+  pinMode(Buzzer, OUTPUT);
+  pinMode(Laser, OUTPUT);
+  pinMode(armedled, OUTPUT);
+}
+
+void loop()
+{
+  Buffer = Serial.read();
+  switch (Buffer) {
+    case 1:
+      digitalWrite(relay1, HIGH);
+      Serial.println("relay1=on");
+      break;
+    case 2:
+      Serial.println('Armed');
+      arm = 1;
+      digitalWrite(armedled, HIGH);
+      digitalWrite(Laser, HIGH);
+      break;
+    case 3:
+      Serial.println('Unarmed');
+      arm = 0;
+      digitalWrite(armedled, LOW);
+      digitalWrite(Laser, LOW);
+      break;
+    case 4:
+      digitalWrite(relay1, LOW);
+      Serial.println("relay1=off");
+      break;
+    case 5:
+      digitalWrite(relay2, HIGH);
+      Serial.println("relay2=on");
+      break;
+    case 6:
+      digitalWrite(relay2, LOW);
+      Serial.println("relay2=off");
+      break;
+    case 7:
+      digitalWrite(relay3, HIGH);
+      Serial.println("relay3=on");
+      break;
+    case 8:
+      digitalWrite(relay3, LOW);
+      Serial.println("relay3=off");
+      break;
+  }
+  LDRvalue = analogRead(LDR);
+  if (LDRvalue == LDRtrigger && arm == 1)
+  {
+    digitalWrite(Buzzer, HIGH);
+    Serial.println('ALARM!!!');
+    delay(delaytime);
+    digitalWrite(Buzzer, LOW);
+  }
+}
+
---
+++ Wireless/Wireless.ino
@@ -0,0 +1,47 @@
+#include  <LiquidCrystal.h>
+#include <DS1307RTC.h>
+#include <Time.h>
+#include <Wire.h>
+#define ARMED 3
+#define MODE 4
+int Buffer = 0;
+int MODENOW = 0;
+LiquidCrystal lcd(8, 12, 9, 10, 11, 12);
+void setup()
+{
+  pinMode(ARMED, OUTPUT);
+  Serial.begin(115200);
+  pinMode(MODE, INPUT_PULLUP);
+  lcd.begin(16, 2);
+  lcd.clear();
+  lcd.print('Welcome');
+  delay(5000);
+  lcd.clear();
+  MODENOW = digitalRead(MODE);
+}
+
+void loop()
+{
+  if (MODENOW == HIGH)
+  {
+    Buffer = Serial.read();
+    if (Buffer == '1')
+    {
+      lcd.clear();
+      lcd.print('ARMED');
+    }
+    if (Buffer == '2')
+    {
+      lcd.clear();
+      lcd.print('Disarmed');
+    }
+    if (Buffer == '3') {
+      lcd.clear();
+      lcd.print('Alarm Activated');
+    }
+  }
+  if (MODENOW == LOW) {
+    lcd.clear();
+    lcd.print(RTC.get());
+  }
+}
